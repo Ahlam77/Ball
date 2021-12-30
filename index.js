2f271b2e7d4c7f6042a0f06bcbaaf302e63d43a2
ahlam alshaba <ama1977ln@outlook.com>
2021-12-30 11:23:26 +0100

    creating a game

---
+++ index.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("bouncingBalls");
+const ctx = canvas.getContext("sd");
+// set width and hight of the canvas 
+const width= canvas.width= window.innerWidth;
+const height = canvas.height=window.innerHeight;
+
+class Ball{
+    constructor(x,y,velX,velY,color,size){
+this.x=x;
+this.y=y;
+this.velX=velX;
+this.velY=velY;
+this.color=color;
+this.size=size;
+
+
+    }
+    draw(){
+        ctx.beginPath(); // start to draw
+        cxt.fillStyle = this.color;
+        ctx.arc(this.x;this.y,this.size, 0,2 *Math.PI);
+        ctx.fill();// finish drawing 
+    }
+    move(){
+        if ((this.x + this.size) >= width){
+            this.velX = -(this.velX);
+        }
+    this.x += this.velX;
+    this.y += this.velY;
+    
+}
+}
